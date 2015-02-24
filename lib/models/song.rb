314eadbc6a1cf408057670528dc6fbbc85bf66c6
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2015-02-24 20:49:20 +0100

    Add as_hash method for songs

--- lib/models/song.rb
+++ lib/models/song.rb
@@ -15,8 +15,15 @@ class Song
 
   attr_accessor :name, :album, :artist, :year, :genre, :length, :price
 
-  def to_s
-    "#{@name} | Album: #{@album} (by #{@artist}) | #{@year} | #{@genre} | #{@price}"
+  def as_hash
+    {}.tap do |hash|
+      self.instance_variables.each do |instance_variable|
+        instance_variable_value     = self.instance_variable_get(instance_variable)
+        formatted_instance_variable = instance_variable.to_s.sub('@', '')
+
+        hash.store(formatted_instance_variable, instance_variable_value)
+      end
+    end
   end
 
 end
