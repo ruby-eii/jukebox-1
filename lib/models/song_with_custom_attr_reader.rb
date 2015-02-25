ded15e5359cb01b4b9f8180e3424c5b8d7e20db2
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2015-02-25 22:57:04 +0100

    Changed SongWithCustomAttrReader's initialize to the same as Song

--- lib/models/song_with_custom_attr_reader.rb
+++ lib/models/song_with_custom_attr_reader.rb
@@ -1,16 +1,13 @@
 class SongWithCustomAttrReader
 
-  def initialize(opts={})
-    # TODO what's the best way for checking that some args should
-    # be passed here? Should we use the method Hash#fetch or checking
-    # the args by hand and launching an exception?
-    @name   = opts[:name]
-    @album  = opts[:album]
-    @artist = opts[:artist]
-    @genre  = opts[:genre]
-    @year   = opts[:year]
-    @length = opts[:length]
-    @price  = opts[:price]
+  def initialize(name, album, artist, opts={})
+    @name   = name
+    @album  = album
+    @artist = artist
+    @genre  = opts["genre"]
+    @year   = opts["year"]
+    @length = opts["length"]
+    @price  = opts["price"]
   end
 
   def method_missing(method, *args, &b)
--- spec/models/song_with_custom_attr_reader_spec.rb
+++ spec/models/song_with_custom_attr_reader_spec.rb
@@ -2,13 +2,15 @@ require "./spec/spec_helper"
 
 describe SongWithCustomAttrReader do
   let(:song){
-    SongWithCustomAttrReader.new :name => "Great Balls of Fire", 
-    :album => "Single Great Ball of Fire",
-    :artist => "Jerry Lee Lewis",
-    :genre => "Rockabilly",
-    :year => 1957,
-    :length => 110,
-    :price => 2.0
+    SongWithCustomAttrReader.new( 
+                                 "Great Balls of Fire", 
+                                 "Single Great Ball of Fire",
+                                 "Jerry Lee Lewis",
+                                 :genre => "Rockabilly",
+                                 :year => 1957,
+                                 :length => 110,
+                                 :price => 2.0
+                                )
   }
 
   describe "#method_missing" do
