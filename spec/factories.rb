5018b7f3171e2989959117f6c6e769fea431b2f4
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2015-02-25 22:52:27 +0100

    Forgotten file in last commit

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,5 +8,15 @@ FactoryGirl.define do
     year   Random.rand(1940..2015)
     length Random.rand(90..240)
     price  Random.rand(2.0..5.0)
+
+    initialize_with { 
+      new(name, album, artist,  
+       "genre" =>  genre,
+       "year" => year,
+       "length" => length,
+       "price" => price
+      )
+    }
+     
   end
 end
