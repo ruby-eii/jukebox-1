2e62daea865242d719ddfcdaec7eeb2edd780fcd
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2015-02-28 23:30:50 +0100

    Player now works using the queue

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,8 +2,11 @@ require "./spec/spec_helper"
 
 describe Player do
   describe "#play" do
-    let(:song1){ Song.new("Mind your manners", "Lightning Bolt", "Pearl Jam", :duration => 200) }
-    let(:song2){ Song.new("Even Flow", "Ten", "Pearl Jam", :duration => 100) }
+    let(:song1){ Song.new("Mind your manners",         "Lightning Bolt", "Pearl Jam",  1, "length" => 200) }
+    let(:song2){ Song.new("Even Flow",                 "Ten",            "Pearl Jam",  1, "length" => 100) }
+    let(:fast_song1){ Song.new("Strutter",             "Greatest Hits",  "Kiss",       1, "length" => 0.1  ) }
+    let(:fast_song2){ Song.new("Rock'n'roll all Nite", "Greatest Hits",  "Kiss",       1, "length" => 0.1  ) }
+
     it "should take a song from the queue" do
       queue = []
       p = Player.new(queue)
@@ -19,6 +22,15 @@ describe Player do
       queue.push(song2)
       Kernel.sleep(0.1)
       expect(queue).to eql([song2])
+    end
+
+    it "should take a song until the last one is ended" do
+      queue = []
+      p = Player.new(queue)
+      queue.push(fast_song1)
+      queue.push(fast_song2)
+      Kernel.sleep(1)
+      expect(queue).to be_empty
     end
   end
 end
