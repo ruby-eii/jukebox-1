9c7d4787ce90613160a5124627c208f8763e3782
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2015-02-28 23:35:02 +0100

    Update Song initialize

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,11 +2,12 @@ require "./spec/spec_helper"
 
 describe Player do
   describe "#play" do
-    let(:song1){ Song.new("Mind your manners", "Lightning Bolt", "Pearl Jam", :duration => 200) }
-    let(:song2){ Song.new("Even Flow", "Ten", "Pearl Jam", :duration => 100) }
+    let(:song1) { Song.new("S000", "Mind your manners", "Lightning Bolt", "Pearl Jam", :duration => 200) }
+    let(:song2) { Song.new("S001", "Even Flow", "Ten", "Pearl Jam", :duration => 100) }
     it "should take a song from the queue" do
       queue = []
-      p = Player.new(queue)
+      Player.new(queue)
+
       queue.push(song1)
       Kernel.sleep(0.1)
       expect(queue).to be_empty
@@ -14,7 +15,8 @@ describe Player do
 
     it "should not take a song until the last one is ended" do
       queue = []
-      p = Player.new(queue)
+      Player.new(queue)
+
       queue.push(song1)
       queue.push(song2)
       Kernel.sleep(0.1)
