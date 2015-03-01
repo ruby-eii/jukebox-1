5126dbba7858e4ed45883eb205d138a3f1819eca
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2015-03-01 13:50:58 +0100

    Player knows what is playing

--- lib/models/player.rb
+++ lib/models/player.rb
@@ -4,19 +4,24 @@ class Player
 
   def initialize(queue)
     @stopped = false
+    @now_playing = nil
     self.play(queue)
   end
 
   def play(queue)
-    Thread.new() do
+    now_playing = @now_playing
+    @thread = Thread.new() do
       while true do
         song = queue.shift
-        @stopped = !!song
+        Thread.current[:now_playing] = song
         Kernel.sleep(song.length) if(song)
       end
     end
   end
 
+  def now_playing
+    @thread[:now_playing]
+  end
 
 end
 
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,11 +1,12 @@
 require "./spec/spec_helper"
 
 describe Player do
+  let(:song1) { Song.new("S000", "Mind your manners", "Lightning Bolt", "Pearl Jam", :duration => 200) }
+  let(:song2) { Song.new("S001", "Even Flow", "Ten", "Pearl Jam", :duration => 100) }
+  let(:fast_song1) { Song.new("S002", "Strutter", "Greatest Hits", "Kiss", 1, "length" => 0.1) }
+  let(:fast_song2) { Song.new("S003", "Rock'n'roll all Nite", "Greatest Hits", "Kiss", 1, "length" => 0.1) }
+
   describe "#play" do
-    let(:song1) { Song.new("S000", "Mind your manners", "Lightning Bolt", "Pearl Jam", :duration => 200) }
-    let(:song2) { Song.new("S001", "Even Flow", "Ten", "Pearl Jam", :duration => 100) }
-    let(:fast_song1) { Song.new("S002", "Strutter", "Greatest Hits", "Kiss", 1, "length" => 0.1) }
-    let(:fast_song2) { Song.new("S003", "Rock'n'roll all Nite", "Greatest Hits", "Kiss", 1, "length" => 0.1) }
 
     it "should take a song from the queue" do
       queue = []
@@ -41,6 +42,29 @@ describe Player do
         Player.new([])
         Kernel.sleep(0.1)
       }.not_to raise_error
+    end
+
+  end
+
+  describe "#now_playing" do
+    it "should know if all songs have ended" do
+      queue = [fast_song1]
+      p = Player.new(queue)
+      Kernel.sleep(0.3)
+      expect(p.now_playing).to be nil
+    end
+    it "should know if no song is played" do
+      queue = []
+      p = Player.new(queue)
+      Kernel.sleep(0.1)
+      expect(p.now_playing).to be nil
+    end
+
+    it "should know what song is being played" do
+      queue = [song1]
+      p = Player.new(queue)
+      Kernel.sleep(0.2)
+      expect(p.now_playing).to be song1
     end
   end
 end
